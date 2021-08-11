83d5e4c5941730d9c3a7433d0bc5bd7d5d5b0977
Ellathet <social.amais@gmail.com>
2021-08-10 23:52:27 -0300

    Fazendo o CRUD de pessoa

--- back/src/controllers/PeopleController.js
+++ back/src/controllers/PeopleController.js
@@ -2,8 +2,52 @@ const knex = require("../database")
 
 module.exports = {
     async index(req, res) {
-       const results = await knex()
+       const results = await knex('people')
 
         return res.json(results)
+    },
+    async create(req, res, next) {
+
+        const { id, name, fun, type, email, cpf, rg, birth, password } = req.body
+
+        try {
+            await knex('people').insert()
+
+            return res.status(201).send()
+
+        } catch (error) {
+            next(error)
+        }           
+    },
+    async update(req, res, next) {
+
+        const { name, fun, type, email, cpf, rg, birth, password } = req.body
+        const { id } = req.params
+
+        try {
+            await knex('people')
+            .update()
+            .where(id)
+
+            return res.status(200).send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async delete(req, res, next) {
+        try {
+
+            const { id } = req.params
+
+            await knex('people')
+                  .where({id})
+                  .del()
+
+            return res.status(200).send()
+
+
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- back/src/routes.js
+++ back/src/routes.js
@@ -5,5 +5,8 @@ const knex = require('./database');
 const PeopleController = require('./controllers/PeopleController') 
 
 routes.get('/people', PeopleController.index)
+routes.post('/people', PeopleController.create)
+routes.put('/people:id', PeopleController.update)
+routes.delete('/people:id', PeopleController.delete)
 
 module.exports = routes
