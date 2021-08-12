09e33c950fa2253e1b9cae3bba1567df294e3cfc
Ellathet <social.amais@gmail.com>
2021-08-12 00:54:55 -0300

    Pagina de Login

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { useContext } from 'react';
+import Router from "next/router"
+import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../contexts/AuthContext';
+import styles from '../styles/Login.module.css'
 
 interface FormData { 
   email: string,
@@ -9,34 +11,68 @@ interface FormData {
 
 export default function Home() {
 
-  const { signIn } = useContext(AuthContext);
+  const [ emptyEmailColor, setEmptyEmailColor ] = useState('')
+  const [ emptyPasswordColor, setEmptyPasswordColor ] = useState('')
+
+  const { signIn, isAuthenticated} = useContext(AuthContext);
 
   const { register, handleSubmit } = useForm()
 
   async function handleSignIn(data : FormData ) {
-    try {
-      await signIn(data)
-    } catch (error) {
-      console.log(error)
+
+    setEmptyEmailColor("")
+    setEmptyPasswordColor("")
+
+    if(data.email === "" && data.password === "" ) {
+      setEmptyEmailColor('#F9BABA')
+      setEmptyPasswordColor('#F9BABA')
+    } else if (data.email === "") {
+      setEmptyEmailColor('#F9BABA')
+    } else if (data.password === "") {
+      setEmptyPasswordColor('#F9BABA')
+    } else {
+      try {
+        await signIn(data)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 
+/*   if(isAuthenticated) {
+    Router.push('/Appointments')
+  } */
+
   return (
     <div>
-      <form id="login" onSubmit={handleSubmit(handleSignIn)}>
-        <label htmlFor="email"> Email</label> 
-        <input type="email" id="email" {...register('email')}/>
-        <label htmlFor="password"> Senha </label>
-        <input type="password" id="password" {...register('password')}/>
-        <div>
-          <div>
-            Registre-se
-          </div>
-          <button type="submit" form="login">
-            Entrar
-          </button>
-        </div>
-      </form>
+        <style>
+          {`
+            #email { background-color: ${emptyEmailColor};}
+            #password { background-color: ${emptyPasswordColor};}
+          `}
+        </style>
+      <div className={styles.header}></div>
+      <div className={styles.container}>
+        <form className={styles.form} id="login" onSubmit={handleSubmit(handleSignIn)}>
+          <h1>Entrar</h1>
+            <div className={styles.inputContainer}>
+              <label htmlFor="email"> Email</label> 
+              <input type="email" id="email" {...register('email')}/>
+            </div>
+            <div className={styles.inputContainer}>    
+              <label htmlFor="password"> Senha </label>
+              <input type="password" id="password" {...register('password')}/>  
+            </div>
+            <div className={styles.buttons}>
+              <button className={styles.register}>
+                Registre-se
+              </button>
+              <button type="submit" form="login" className={styles.enter}>
+                Entrar
+              </button>
+            </div>
+        </form>
+      </div>
     </div>
   )
 }
